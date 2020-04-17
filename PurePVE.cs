c14e50177b7402f50853d4bcc717c90e84fc5ca7
Administrator <admin@example.com>
2020-04-17 17:51:45 -0500

    Add a ton of GUI which may help inform operation somewhat.

--- PurePVE.cs
+++ PurePVE.cs
@@ -1,9 +1,9 @@
-//#define DEBUG
 using System;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using Oxide.Core;
 using Oxide.Core.Configuration;
@@ -17,7 +17,7 @@ using Oxide.Game.Rust.Cui;
 
 namespace Oxide.Plugins
 {
-    [Info("Pure PVE", "RFC1920", "1.0.2")]
+    [Info("Pure PVE", "RFC1920", "1.0.3")]
     [Description("Prevent damage to players and objects in a PVE environment")]
     class PurePVE : RustPlugin
     {
@@ -29,6 +29,7 @@ namespace Oxide.Plugins
 
         private const string permPurePVEUse = "purepve.use";
         private const string permPurePVEAdmin = "purepve.admin";
+        private const string permPurePVEGod = "purepve.god";
         private ConfigData configData;
 
         [PluginReference]
@@ -36,6 +37,10 @@ namespace Oxide.Plugins
 
         private string logfilename = "log";
         private bool dolog = false;
+
+        const string PPVEL = "purepve.rulelist";
+        const string PPVERS = "purepve.ruleseteditor";
+        const string PPVER = "purepve.ruleeditor";
         #endregion
 
         #region Message
@@ -48,8 +53,10 @@ namespace Oxide.Plugins
         {
             LoadConfigVariables();
             AddCovalenceCommand("pvelog", "cmdPurePVElog");
+            AddCovalenceCommand("pverule", "cmdPurePVEGUI");
             permission.RegisterPermission(permPurePVEUse, this);
             permission.RegisterPermission(permPurePVEAdmin, this);
+            permission.RegisterPermission(permPurePVEGod, this);
         }
 
         void OnServerInitialized()
@@ -58,9 +65,39 @@ namespace Oxide.Plugins
             lang.RegisterMessages(new Dictionary<string, string>
             {
                 ["notauthorized"] = "You don't have permission to use this command.",
+                ["purepverulesets"] = "PurePVE Rulesets",
+                ["purepveruleset"] = "PurePVE Ruleset",
+                ["purepverule"] = "PurePVE Rule",
+                ["close"] = "Close",
+                ["save"] = "Save",
+                ["edit"] = "Edit",
+                ["add"] = "Add",
+                ["true"] = "True",
+                ["false"] = "False",
+                ["editing"] = "Editing",
+                ["exclude"] = "Exclude",
+                ["zone"] = "Zone",
+                ["schedule"] = "Schedule",
+                ["editing"] = "Editing",
+                ["select"] = "Select",
+                ["damage"] = "Damage",
+                ["defaultdamage"] = "Default Damage",
+                ["damageexceptions"] = "Damage Exceptions",
+                ["rule"] = "Rule",
+                ["rules"] = "Rules",
                 ["logging"] = "Logging set to {0}"
             }, this);
         }
+
+		void Unload()
+		{
+            foreach(BasePlayer player in BasePlayer.activePlayerList)
+            {
+                CuiHelper.DestroyUi(player, PPVEL);
+                CuiHelper.DestroyUi(player, PPVER);
+                CuiHelper.DestroyUi(player, PPVERS);
+			}
+		}
 
         void LoadData()
         {
@@ -182,7 +219,7 @@ namespace Oxide.Plugins
 
             if(stype == "BasePlayer")
             {
-                if((hitInfo.Initiator as BasePlayer).IPlayer.HasPermission(permPurePVEAdmin))
+                if((hitInfo.Initiator as BasePlayer).IPlayer.HasPermission(permPurePVEGod))
                 {
                     Puts("Admin god!");
                     return null;
@@ -212,7 +249,6 @@ namespace Oxide.Plugins
             // Special case since HumanNPC contains a BasePlayer object
             if(stype == "BasePlayer" && HumanNPC && IsHumanNPC(source)) stype = "HumanNPC";
             if(ttype == "BasePlayer" && HumanNPC && IsHumanNPC(target)) ttype = "HumanNPC";
-
 
             //var turret = source.Weapon?.GetComponentInParent<AutoTurret>();
 
@@ -337,10 +373,200 @@ namespace Oxide.Plugins
         [Command("pvelog")]
         void cmdPurePVElog(IPlayer player, string command, string[] args)
         {
-            if(!player.HasPermission(permPurePVEUse)) { Message(player, "notauthorized"); return; }
+            if(!player.HasPermission(permPurePVEAdmin)) { Message(player, "notauthorized"); return; }
 
             dolog = !dolog;
             Message(player, "logging", dolog.ToString());
+        }
+
+        [Command("pverule")]
+        void cmdPurePVEGUI(IPlayer iplayer, string command, string[] args)
+        {
+            if(!iplayer.HasPermission(permPurePVEAdmin)) { Message(iplayer, "notauthorized"); return; }
+            var player = iplayer.Object as BasePlayer;
+
+            if(args.Length > 0)
+            {
+                switch(args[0])
+                {
+					case "editrule":
+						Puts($"editrule {args[1]}");
+						GUIRulesetEditor(player, args[1]);
+						break;
+                    case "close":
+						Puts("close");
+                        CuiHelper.DestroyUi(player, PPVEL);
+                        CuiHelper.DestroyUi(player, PPVER);
+                        CuiHelper.DestroyUi(player, PPVERS);
+                        break;
+                    case "closeruleset":
+						Puts("closeruleset");
+                        CuiHelper.DestroyUi(player, PPVERS);
+                        break;
+                    case "closerule":
+						Puts("closerule");
+                        CuiHelper.DestroyUi(player, PPVER);
+                        break;
+                    default:
+                        GUIRuleSets(player);
+                        break;
+                }
+            }
+			else
+			{
+	            GUIRuleSets(player);
+			}
+        }
+
+        void GUIRuleSets(BasePlayer player)
+        {
+            CuiHelper.DestroyUi(player, PPVEL);
+
+            CuiElementContainer container = UI.Container(PPVEL, UI.Color("2b2b2b", 1f), "0.05 0.05", "0.95 0.95", true, "Overlay");
+            UI.Button(ref container, PPVEL, UI.Color("#d85540", 1f), Lang("close"), 12, "0.93 0.95", "0.99 0.98", $"pverule close");
+            UI.Label(ref container, PPVEL, UI.Color("#ffffff", 1f), Lang("purepverulesets") + ": ", 24, "0.2 0.92", "0.7 1");
+
+            int col = 0;
+            int row = 0;
+			float[] pb;
+            foreach(KeyValuePair<string, PurePVERuleSet> ruleset in pverulesets)
+			{
+                if(row > 10)
+                {
+                    row = 0;
+                    col++;
+                }
+                pb = GetButtonPositionP(row, col);
+
+                UI.Button(ref container, PPVEL, UI.Color("#d85540", 1f), ruleset.Key, 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule {ruleset.Key}");
+			}
+			row++;
+			pb = GetButtonPositionP(row, col);
+			UI.Button(ref container, PPVEL, UI.Color("#55d840", 1f), Lang("add"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule add");
+
+            CuiHelper.AddUi(player, container);
+        }
+
+        void GUIRulesetEditor(BasePlayer player, string rulesetname)
+        {
+            CuiHelper.DestroyUi(player, PPVERS);
+
+            CuiElementContainer container = UI.Container(PPVERS, UI.Color("2b2b2b", 1f), "0.05 0.05", "0.95 0.95", true, "Overlay");
+            UI.Button(ref container, PPVERS, UI.Color("#d85540", 1f), Lang("close"), 12, "0.93 0.95", "0.99 0.98", $"pverule closeruleset");
+            UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("purepveruleset") + ": " + rulesetname, 24, "0.2 0.92", "0.7 1");
+
+			string dicolor = "#cccccc";
+			string encolor = "#ffcccc";
+            int col = 0;
+            int row = 0;
+
+            float[] pb = GetButtonPositionP(row, col);
+            UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("defaultdamage"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}");
+
+			col++;
+			pb = GetButtonPositionP(row, col);
+			UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("damageexceptions"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}");
+
+			col++;
+			pb = GetButtonPositionP(row, col);
+			UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("exclude"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}");
+
+			col++;
+			pb = GetButtonPositionP(row, col);
+			UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("zone"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}");
+
+			col++;
+			pb = GetButtonPositionP(row, col);
+			UI.Label(ref container, PPVERS, UI.Color("#ffffff", 1f), Lang("schedule"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}");
+
+			row++;
+			col = 0;
+
+			pb = GetButtonPositionP(row, col);
+			if(pverulesets[rulesetname].damage)
+			{
+				UI.Button(ref container, PPVERS, UI.Color(encolor, 1f), Lang("true"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editruleset {rulesetname} damage 0");
+			}
+			else
+			{
+				UI.Button(ref container, PPVERS, UI.Color(dicolor, 1f), Lang("false"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editruleset {rulesetname} damage 1");
+			}
+
+			col++;
+			foreach(string except in pverulesets[rulesetname].except)
+			{
+                pb = GetButtonPositionP(row, col);
+				UI.Button(ref container, PPVERS, UI.Color("#d85540", 1f), except, 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule except {except}");
+				row++;
+			}
+			pb = GetButtonPositionP(row, col);
+			UI.Button(ref container, PPVERS, UI.Color("#55d840", 1f), Lang("add"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule except add");
+
+			col++; row = 1;
+			foreach(string exclude in pverulesets[rulesetname].exclude)
+			{
+                if(row > 10)
+                {
+                    row = 0;
+                    col++;
+                }
+				pb = GetButtonPositionP(row, col);
+				UI.Button(ref container, PPVERS, UI.Color("#d85540", 1f), exclude, 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule exclude {exclude}");
+				row++;
+			}
+			pb = GetButtonPositionP(row, col);
+			UI.Button(ref container, PPVERS, UI.Color("#55d840", 1f), Lang("add"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule exclude add");
+
+			col++; row = 1;
+			pb = GetButtonPositionP(row, col);
+			if(pverulesets[rulesetname].zone != null)
+			{
+				UI.Button(ref container, PPVERS, UI.Color("#d85540", 1f), pverulesets[rulesetname].zone, 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule zone {pverulesets[rulesetname].zone}");
+			}
+			else
+			{
+				UI.Button(ref container, PPVERS, UI.Color("#55d840", 1f), Lang("default"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule zone add}");
+			}
+
+			col++; row = 1;
+			pb = GetButtonPositionP(row, col);
+			if(pverulesets[rulesetname].zone != null)
+			{
+				UI.Button(ref container, PPVERS, UI.Color("#d85540", 1f), pverulesets[rulesetname].schedule, 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule schedule {pverulesets[rulesetname].schedule}");
+			}
+			else
+			{
+				UI.Button(ref container, PPVERS, UI.Color("#55d840", 1f), Lang("add"), 12, $"{pb[0]} {pb[1]}", $"{pb[0] + ((pb[2] - pb[0]) / 2)} {pb[3]}", $"pverule editrule schedule add}");
+			}
+
+            CuiHelper.AddUi(player, container);
+        }
+
+        void GUIRuleEditor(BasePlayer player, string rulename)
+        {
+            CuiHelper.DestroyUi(player, PPVER);
+
+            CuiElementContainer container = UI.Container(PPVER, UI.Color("2b2b2b", 1f), "0.05 0.05", "0.95 0.95", true, "Overlay");
+            UI.Button(ref container, PPVER, UI.Color("#d85540", 1f), Lang("close"), 12, "0.93 0.95", "0.99 0.98", $"pverule closerule");
+            UI.Label(ref container, PPVER, UI.Color("#ffffff", 1f), Lang("purepverule") + ": " + rulename, 24, "0.2 0.92", "0.7 1");
+
+            CuiHelper.AddUi(player, container);
+        }
+
+        private int RowNumber(int max, int count) => Mathf.FloorToInt(count / max);
+        private float[] GetButtonPosition(int rowNumber, int columnNumber)
+        {
+            float offsetX = 0.05f + (0.096f * columnNumber);
+            float offsetY = (0.80f - (rowNumber * 0.064f));
+
+            return new float[] { offsetX, offsetY, offsetX + 0.196f, offsetY + 0.03f };
+        }
+        private float[] GetButtonPositionP(int rowNumber, int columnNumber)
+        {
+            float offsetX = 0.05f + (0.126f * columnNumber);
+            float offsetY = (0.87f - (rowNumber * 0.064f));
+
+            return new float[] { offsetX, offsetY, offsetX + 0.226f, offsetY + 0.03f };
         }
         #endregion
 
@@ -458,6 +684,113 @@ namespace Oxide.Plugins
         public class PurePVEEntities
         {
             public List<string> types;
+        }
+
+        public static class UI
+        {
+            public static CuiElementContainer Container(string panel, string color, string min, string max, bool useCursor = false, string parent = "Overlay")
+            {
+                CuiElementContainer container = new CuiElementContainer()
+                {
+                    {
+                        new CuiPanel
+                        {
+                            Image = { Color = color },
+                            RectTransform = {AnchorMin = min, AnchorMax = max},
+                            CursorEnabled = useCursor
+                        },
+                        new CuiElement().Parent = parent,
+                        panel
+                    }
+                };
+                return container;
+            }
+            public static void Panel(ref CuiElementContainer container, string panel, string color, string min, string max, bool cursor = false)
+            {
+                container.Add(new CuiPanel
+                {
+                    Image = { Color = color },
+                    RectTransform = { AnchorMin = min, AnchorMax = max },
+                    CursorEnabled = cursor
+                },
+                panel);
+            }
+            public static void Label(ref CuiElementContainer container, string panel, string color, string text, int size, string min, string max, TextAnchor align = TextAnchor.MiddleCenter)
+            {
+                container.Add(new CuiLabel
+                {
+                    Text = { Color = color, FontSize = size, Align = align, Text = text },
+                    RectTransform = { AnchorMin = min, AnchorMax = max }
+                },
+                panel);
+
+            }
+            public static void Button(ref CuiElementContainer container, string panel, string color, string text, int size, string min, string max, string command, TextAnchor align = TextAnchor.MiddleCenter)
+            {
+                container.Add(new CuiButton
+                {
+                    Button = { Color = color, Command = command, FadeIn = 0f },
+                    RectTransform = { AnchorMin = min, AnchorMax = max },
+                    Text = { Text = text, FontSize = size, Align = align }
+                },
+                panel);
+            }
+            public static void Input(ref CuiElementContainer container, string panel, string color, string text, int size, string min, string max, string command, TextAnchor align = TextAnchor.MiddleCenter)
+            {
+                container.Add(new CuiElement
+                {
+                    Name = CuiHelper.GetGuid(),
+                    Parent = panel,
+                    Components =
+                    {
+                        new CuiInputFieldComponent
+                        {
+                            Align = align,
+                            CharsLimit = 30,
+                            Color = color,
+                            Command = command + text,
+                            FontSize = size,
+                            IsPassword = false,
+                            Text = text
+                        },
+                        new CuiRectTransformComponent { AnchorMin = min, AnchorMax = max },
+                        new CuiNeedsCursorComponent()
+                    }
+                });
+            }
+            public static void Icon(ref CuiElementContainer container, string panel, string color, string imageurl, string min, string max)
+            {
+                container.Add(new CuiElement
+                {
+                    Name = CuiHelper.GetGuid(),
+                    Parent = panel,
+                    Components =
+                    {
+                        new CuiRawImageComponent
+                        {
+                            Url = imageurl,
+                            Sprite = "assets/content/textures/generic/fulltransparent.tga",
+                            Color = color
+                        },
+                        new CuiRectTransformComponent
+                        {
+                            AnchorMin = min,
+                            AnchorMax = max 
+                        }
+                    }
+                });
+            }
+            public static string Color(string hexColor, float alpha)
+            {
+                if(hexColor.StartsWith("#"))
+                {
+                    hexColor = hexColor.Substring(1);
+                }
+                int red = int.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+                int green = int.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+                int blue = int.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+                return $"{(double)red / 255} {(double)green / 255} {(double)blue / 255} {alpha}";
+            }
         }
         #endregion
 
